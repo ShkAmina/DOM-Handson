b49d81e89f3ae1606f5d2075949f9e41933e3c80
sara <shksara2000@gmail.com>
2023-04-27 00:55:27 +0530

    DOM Handson

--- Gamescript.js
+++ Gamescript.js
@@ -2,30 +2,35 @@ let randomNumber = parseInt(Math.random() *100) + 1;
 console.log(randomNumber)
 
 var limit = 100;
+
+let box = document.getElementById('box')
+let score = document.getElementById('score')
+let text = document.getElementById('Text')
+let number = document.getElementById('Number')
+let chance = document.getElementById('chance')
+let body = document.getElementById('body')
+
 function changeme()
 {
     // let  Input = document.getElementById('Number');
-    let text = document.getElementById('Text')
-    let chance = document.getElementById('chance')
-    let Number = document.getElementById('Number')
-    let body = document.getElementById('body')
-    let score = document.getElementById('score')
-    let box = document.getElementById('box')
 
-    if(randomNumber > parseInt(Number.value))
+    if(randomNumber > parseInt(number.value))
     {
    
         text.innerHTML = 'You are too Low!';
         chance.innerText = `💯 Chance : ${--limit}`;
         chance.style.fontSize = '25px'
+        number.style.color = "black";
         
     }
-    else if(randomNumber < parseInt(Number.value))
+    else if(randomNumber < parseInt(number.value))
     {
         
         text.innerHTML = 'You are too High!';
         chance.innerText = `💯 Chance : ${--limit}`;
         chance.style.fontSize = '25px'
+        number.style.color = "black";
+
     }
     else 
     {
@@ -34,10 +39,24 @@ function changeme()
         chance.innerText = chance.innerHTML;
         body.style.backgroundColor = "green";
         score.innerText = ` 🥇 Highscore : ${limit}`;
-        Number.style.backgroundColor = "green";
-        Number.style.color = "white";
+        number.style.backgroundColor = "green";
+        number.style.color = "white";
         score.style.fontSize  = '25px';
-        box.innerText = Number.value;
+        box.innerText = number.value;
 
     }
 }
+let resetButton = document.querySelector("#Play");
+resetButton.addEventListener("click", () => {
+    randomNumber = parseInt(Math.random() *100) + 1;
+    text.innerHTML = 'Start guessing...';
+    number.value = "";
+    box.innerText = '?';
+    score.innerText = `🥇 Highscore : 0 `;
+    chance.innerText = `💯 Chances : 100`;
+    body.style.backgroundColor = "#2A2F4F";
+    number.style.backgroundColor = "white";
+    number.style.color = "black";
+
+   
+  });
